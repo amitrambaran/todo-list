40d3f0f31359fdd4d7fb4f4512c7b6dfdb1456c7
amitrambaran <amit.rambaran@gmail.com>
2019-01-30 22:15:47 -0500

    add basic add/remove functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
-$('li').click(function () {
+$('ul').on("click", "li", function() {
+    $(this).toggleClass("complete");
+});
 
+$('ul').on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function () {
+        remove();
+    });
+    event.stopPropagation();
+});
 
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).css("color", "black");
-    }
-    else {
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
     }
 });
